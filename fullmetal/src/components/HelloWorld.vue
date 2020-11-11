c6cfe9c0e8c139971d49909abbb70ed1e7fde717
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2020-11-11 16:44:45 -0300

    vue-router

--- fullmetal/src/components/HelloWorld.vue
+++ fullmetal/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-for="(c,idx) in cor" :key="idx">{{ cor[idx] + ' '+ idx }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +34,8 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    cor: [],
   }
 }
 </script>
---
+++ fullmetal/src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="flex-center">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title" style="font-size:50px;">Bem-vindo(a) a <strong>Amestris</strong>!</h5>
+                    <div class="links">
+                        <a type="button" class="item">Entrar </a>
+                        <a type="button" class="item"> Cadastrar-se </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style>
+   .item{
+        position: relative; font-size: 20px; line-height: 14px; text-decoration: none;
+        transition: 0.4s ease;
+    }
+    .item:hover{
+        color: #313238; font-style: italic; text-decoration: underline; font-size: 24px;
+    }
+    .item:hover::after{
+        color: #313238; font-style: italic; text-decoration: underline; font-size: 24px;
+    }
+
+    .links{
+        color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+    }
+</style>
